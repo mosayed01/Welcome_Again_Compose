9c577b096203b85defefb0cb381b0b1568fb9693
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-06-29 17:56:04 +0300

    add: TimerProgress using Canvas

--- app/src/main/java/com/example/welcomecompose/MainActivity.kt
+++ app/src/main/java/com/example/welcomecompose/MainActivity.kt
@@ -1,50 +1,65 @@
 package com.example.welcomecompose
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.UiMode
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.welcomecompose.presentation.components.timer.TimerProgress
 import com.example.welcomecompose.presentation.ui.theme.WelcomeComposeTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             WelcomeComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
                 ) {
-                    Greeting("Android")
+                    var countDownTimer by remember { mutableStateOf(30) }
+
+                    LaunchedEffect(key1 = Unit){
+                        while (true){
+                            if (countDownTimer == 0){
+                                break
+                            }
+                            delay(1000)
+                            countDownTimer--
+                        }
+                    }
+
+                    TimerProgress(
+                        maxTime = 30,
+                        currentTime = countDownTimer,
+                        progressColor = Color.Blue,
+                        modifier = Modifier.size(50.dp)
+                    ) {
+                        Text(
+                            text = "$countDownTimer",
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = Color.Blue.copy(alpha = 0.87f)
+                        )
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES, showSystemUi = true)
-@Composable
-fun GreetingPreview() {
-    WelcomeComposeTheme {
-        Surface {
-            Greeting("Android")
-        }
-    }
-}
