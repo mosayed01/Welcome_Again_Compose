0b3f1996cc88386c4d42e72b0352ca453ba230c2
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-06-30 16:35:10 +0300

    test: in main activity

--- app/src/main/java/com/example/welcomecompose/MainActivity.kt
+++ app/src/main/java/com/example/welcomecompose/MainActivity.kt
@@ -8,19 +8,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.example.welcomecompose.presentation.components.timer.TimerProgress
+import com.example.welcomecompose.presentation.components.timer.ColoredTimerProgress
 import com.example.welcomecompose.presentation.ui.theme.WelcomeComposeTheme
-import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,34 +21,15 @@ class MainActivity : ComponentActivity() {
             WelcomeComposeTheme {
                 Row(
                     horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
                     modifier = Modifier
                         .fillMaxSize()
                         .padding(16.dp),
                 ) {
-                    var countDownTimer by remember { mutableStateOf(30) }
-
-                    LaunchedEffect(key1 = Unit){
-                        while (true){
-                            if (countDownTimer == 0){
-                                break
-                            }
-                            delay(1000)
-                            countDownTimer--
-                        }
-                    }
-
-                    TimerProgress(
+                    ColoredTimerProgress(
                         maxTime = 30,
-                        currentTime = countDownTimer,
-                        progressColor = Color.Blue,
-                        modifier = Modifier.size(50.dp)
-                    ) {
-                        Text(
-                            text = "$countDownTimer",
-                            style = MaterialTheme.typography.bodyMedium,
-                            color = Color.Blue.copy(alpha = 0.87f)
-                        )
-                    }
+                        modifier = Modifier.size(250.dp)
+                    )
                 }
             }
         }
