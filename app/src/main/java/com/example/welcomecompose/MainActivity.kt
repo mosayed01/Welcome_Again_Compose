88232530fd34fe98f9137c8f320f4a36a68f0aa0
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-07-02 19:30:41 +0300

    add buy tickets screen

--- app/src/main/java/com/example/welcomecompose/MainActivity.kt
+++ app/src/main/java/com/example/welcomecompose/MainActivity.kt
@@ -3,15 +3,7 @@ package com.example.welcomecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.example.welcomecompose.presentation.components.timer.ColoredTimerProgress
+import com.example.welcomecompose.presentation.BookingScreen
 import com.example.welcomecompose.presentation.ui.theme.WelcomeComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             WelcomeComposeTheme {
-
+                BookingScreen()
             }
         }
     }
--- app/src/main/java/com/example/welcomecompose/presentation/components/DateChip.kt
+++ app/src/main/java/com/example/welcomecompose/presentation/components/DateChip.kt
@@ -6,21 +6,24 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.welcomecompose.presentation.ui.theme.Black8
 import com.example.welcomecompose.presentation.ui.theme.Black87
 import com.example.welcomecompose.presentation.ui.theme.DarkGray
 import com.example.welcomecompose.presentation.ui.theme.LightGray
 import com.example.welcomecompose.presentation.ui.theme.Sans
 
+
+data class Day(val dayNumber: Int, val dayName: String)
+
 @Composable
 fun DateChip(
-    dayNumber: Int,
-    dayName: String,
+    day: Day,
     modifier: Modifier = Modifier,
     isSelected: Boolean = false
 ) {
     PrimaryChip(
-        text = dayNumber.toString(),
+        text = day.dayNumber.toString(),
         isSelected = isSelected,
         unSelectedTextColor = Black87,
         backgroundColors = listOf(DarkGray, LightGray),
@@ -28,10 +31,11 @@ fun DateChip(
         modifier = modifier.padding(horizontal = 8.dp)
     ) {
         Text(
-            text = dayName,
-            color = it.copy(0.6f),
+            text = day.dayName,
+            color = it.copy(0.38f),
             fontFamily = Sans,
-            fontWeight = FontWeight.Normal
+            fontWeight = FontWeight.Normal,
+            fontSize = 11.sp
         )
     }
 }
