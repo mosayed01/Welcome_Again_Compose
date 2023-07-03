10ccde17b18428ca5689572db0ea002f1d496ddc
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-07-04 00:13:49 +0300

    refactor home screen

--- app/src/main/java/com/example/welcomecompose/presentation/screens/HomeScreen.kt
+++ app/src/main/java/com/example/welcomecompose/presentation/screens/HomeScreen.kt
@@ -13,15 +13,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
@@ -56,11 +53,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.welcomecompose.R
 import com.example.welcomecompose.presentation.composables.PrimaryChip
+import com.example.welcomecompose.presentation.screens.util.Space
 import com.example.welcomecompose.presentation.ui.theme.Black38
 import com.example.welcomecompose.presentation.ui.theme.Black8
 import com.example.welcomecompose.presentation.ui.theme.Black87
-import com.example.welcomecompose.presentation.ui.theme.DarkGray
-import com.example.welcomecompose.presentation.ui.theme.LightGray
 import com.example.welcomecompose.presentation.ui.theme.OnPrimaryLight
 import com.example.welcomecompose.presentation.ui.theme.PrimaryLight
 import com.example.welcomecompose.presentation.ui.theme.Sans
@@ -79,27 +75,12 @@ fun HomeScreen() {
             ) {
                 NavigationItem(painter = painterResource(id = R.drawable.movie), isSelected = true)
                 NavigationItem(painter = painterResource(id = R.drawable.ic_search))
-                NavigationItem(painter = painterResource(id = R.drawable.ticket)) {
-                    Text(
-                        text = "5",
-                        fontFamily = Sans,
-                        fontWeight = FontWeight.Normal,
-                        textAlign = TextAlign.Center,
-                        fontSize = 12.sp,
-                        color = OnPrimaryLight,
-                        modifier = Modifier
-                            .size(18.dp)
-                            .clip(CircleShape)
-                            .background(PrimaryLight),
-                        )
-                }
+                NavigationItem(painter = painterResource(id = R.drawable.ticket)) { Badge(count = 5) }
                 NavigationItem(painter = painterResource(id = R.drawable.user))
             }
         },
     ) {
-        Box(
-            modifier = Modifier.fillMaxSize()
-        ) {
+        Box(modifier = Modifier.fillMaxSize()) {
             val pagerState = rememberPagerState(initialPage = 1)
             val images = listOf(
                 R.drawable.img_1,
@@ -109,91 +90,120 @@ fun HomeScreen() {
             Crossfade(
                 targetState = images[pagerState.currentPage],
                 animationSpec = tween(200, easing = FastOutSlowInEasing),
-            ) {
-                BackgroundWithBlurredImage(painter = painterResource(id = it))
-            }
-            Column(
-                modifier = Modifier
-                    .matchParentSize()
-                    .padding(it), // ! I couldn't use paddingValues for Scaffold
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Spacer(modifier = Modifier.height(24.dp))
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center,
-                ) {
-                    PrimaryChip(
-                        text = "New Showing",
-                        isSelected = true,
-                        modifier = Modifier.padding(horizontal = 8.dp),
-                    )
-                    Spacer(modifier = Modifier.width(8.dp))
-                    PrimaryChip(
-                        text = "Coming Soon",
-                        isSelected = false,
-                        modifier = Modifier.padding(horizontal = 8.dp),
-                    )
-                }
-                Spacer(modifier = Modifier.height(24.dp))
-                HorizontalImages(pagerState = pagerState, images = images)
-                Spacer(modifier = Modifier.height(24.dp))
-
-                Row(
-                    modifier = Modifier.padding(4.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.time),
-                        contentDescription = "date",
-                        tint = Black38,
-                        modifier = Modifier.size(16.dp),
-                    )
-                    Spacer(modifier = Modifier.width(4.dp))
-                    Text(
-                        text = "2h 23m",
-                        fontFamily = Sans,
-                        fontWeight = FontWeight.Normal,
-                        color = Black87,
-                        fontSize = 12.sp
-                    )
-                }
-                Spacer(modifier = Modifier.height(16.dp))
-
-                Text(
-                    text = "Fantastic Beasts: The \nSecrets of Dumbledore",
-                    fontFamily = Sans,
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight.Medium,
-                    textAlign = TextAlign.Center,
-                )
-
-                Spacer(modifier = Modifier.height(16.dp))
-
-                Row {
-                    PrimaryChip(
-                        text = "Fantasy",
-                        isEnabled = false,
-                        unSelectedTextColor = Black87,
-                        backgroundColors = listOf(DarkGray, LightGray),
-                        borderColor = Black8,
-                        fontSize = 12.sp
-                    )
-                    Spacer(modifier = Modifier.width(4.dp))
-                    PrimaryChip(
-                        text = "Adventure",
-                        isEnabled = false,
-                        unSelectedTextColor = Black87,
-                        backgroundColors = listOf(DarkGray, LightGray),
-                        borderColor = Black8,
-                        fontSize = 12.sp
-                    )
-                }
-            }
+            ) { BackgroundWithBlurredImage(painter = painterResource(id = it)) }
+            HomeScreenContent(paddingValues = it, pagerState = pagerState, images = images)
         }
-
     }
+}
+
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun HomeScreenContent(
+    paddingValues: PaddingValues,
+    pagerState: PagerState,
+    images: List<Int>
+
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(paddingValues), // ! I couldn't use paddingValues for Scaffold
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Space(space = 24)
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            PrimaryChip(
+                text = "New Showing",
+                isSelected = true,
+                modifier = Modifier.padding(horizontal = 8.dp),
+            )
+            Space(space = 8)
+            PrimaryChip(
+                text = "Coming Soon",
+                isSelected = false,
+                modifier = Modifier.padding(horizontal = 8.dp),
+            )
+        }
+        Space(space = 24)
+
+        HorizontalImages(pagerState = pagerState, images = images)
+
+        Space(space = 24)
+
+        Row(
+            modifier = Modifier.padding(4.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.time),
+                contentDescription = "time",
+                tint = Black38,
+                modifier = Modifier.size(16.dp),
+            )
+            Space(space = 4)
+            Text(
+                text = "2h 23m",
+                fontFamily = Sans,
+                fontWeight = FontWeight.Normal,
+                color = Black87,
+                fontSize = 12.sp
+            )
+        }
+        Space(space = 16)
+
+        Text(
+            text = "Fantastic Beasts: The \nSecrets of Dumbledore",
+            fontFamily = Sans,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Medium,
+            textAlign = TextAlign.Center,
+        )
+
+        Space(space = 16)
+
+        Row {
+            PrimaryChip(
+                text = "Fantasy",
+                isEnabled = false,
+                unSelectedTextColor = Black87,
+                borderColor = Black8,
+                fontSize = 12.sp
+            )
+            Space(space = 4)
+            PrimaryChip(
+                text = "Adventure",
+                isEnabled = false,
+                unSelectedTextColor = Black87,
+                borderColor = Black8,
+                fontSize = 12.sp
+            )
+        }
+    }
+}
+
+
+@Composable
+fun Badge(
+    count: Int,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = count.toString(),
+        fontFamily = Sans,
+        fontWeight = FontWeight.Normal,
+        textAlign = TextAlign.Center,
+        fontSize = 12.sp,
+        color = OnPrimaryLight,
+        modifier = modifier
+            .size(18.dp)
+            .clip(CircleShape)
+            .background(PrimaryLight),
+    )
 }
 
 @Composable
@@ -221,7 +231,7 @@ fun RowScope.NavigationItem(
                     tint = if (isSelected) OnPrimaryLight else Black87
                 )
                 moreContent?.let {
-                    Spacer(modifier = Modifier.width(4.dp))
+                    Space(space = 4)
                     it()
                 }
             }
--- app/src/main/java/com/example/welcomecompose/presentation/screens/util/Excetentions.kt
+++ app/src/main/java/com/example/welcomecompose/presentation/screens/util/Excetentions.kt
@@ -1,10 +1,38 @@
 package com.example.welcomecompose.presentation.screens.util
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 fun Modifier.clickableIf(condition: () -> Boolean, onClick: (() -> Unit)? = null): Modifier {
     return if (condition()) {
         then(Modifier.clickable { onClick?.invoke() })
     } else this
 }
+
+@Composable
+fun ColumnScope.Space(space: Int){
+    Spacer(modifier = Modifier.height(space.dp))
+}
+
+@Composable
+fun RowScope.Space(space: Int){
+    Spacer(modifier = Modifier.width(space.dp))
+}
+
+@Composable
+fun RowScope.WeightedSpacer(weight: Float) {
+    Spacer(modifier = Modifier.weight(weight))
+}
+
+@Composable
+fun ColumnScope.WeightedSpacer(weight: Float) {
+    Spacer(modifier = Modifier.weight(weight))
+}
+
