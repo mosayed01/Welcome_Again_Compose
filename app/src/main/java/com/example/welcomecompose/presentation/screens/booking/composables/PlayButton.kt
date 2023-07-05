6d9538dbf061f0b0d24cd806bbe7df10214cde8d
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-07-05 18:31:16 +0300

    use box instead of icon button for solve the ripple color issue

--- app/src/main/java/com/example/welcomecompose/presentation/screens/booking/composables/PlayButton.kt
+++ app/src/main/java/com/example/welcomecompose/presentation/screens/booking/composables/PlayButton.kt
@@ -1,43 +1,47 @@
 package com.example.welcomecompose.presentation.screens.booking.composables
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.welcomecompose.R
 import com.example.welcomecompose.presentation.ui.theme.OnPrimaryLight
 import com.example.welcomecompose.presentation.ui.theme.PrimaryLight
 
 @Composable
-fun PlayButton() {
-    IconButton(
-        onClick = { /*TODO*/ },
-        modifier = Modifier.size(56.dp)
+fun PlayButton(
+    onClick: () -> Unit = {}
+) {
+    Box(
+        modifier = Modifier
+            .size(56.dp)
+            .clip(CircleShape)
+            .clickable { onClick() }
+            .background(PrimaryLight)
+            .padding(8.dp),
+        contentAlignment = Alignment.Center
     ) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .clip(CircleShape)
-                .background(PrimaryLight)
-                .padding(8.dp),
-            contentAlignment = Alignment.Center
-        ) {
-            Icon(
-                painter = painterResource(id = R.drawable.ic_poly),
-                contentDescription = "",
-                tint = OnPrimaryLight,
-                modifier = Modifier.size(24.dp)
-            )
-        }
+        Icon(
+            painter = painterResource(id = R.drawable.ic_poly),
+            contentDescription = "",
+            tint = OnPrimaryLight,
+            modifier = Modifier.size(24.dp)
+        )
     }
+}
+
+@Preview
+@Composable
+fun PlayButtonPrev() {
+    PlayButton()
 }
