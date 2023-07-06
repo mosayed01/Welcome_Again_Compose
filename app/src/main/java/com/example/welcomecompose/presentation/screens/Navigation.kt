d17d1e9394fb2c91191b0a28a92ac476ce6049c3
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-07-06 17:15:33 +0300

    handle bottom navigation animation

--- app/src/main/java/com/example/welcomecompose/presentation/screens/Navigation.kt
+++ app/src/main/java/com/example/welcomecompose/presentation/screens/Navigation.kt
@@ -1,14 +1,20 @@
 package com.example.welcomecompose.presentation.screens
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.welcomecompose.presentation.screens.booking.BookingScreen
 import com.example.welcomecompose.presentation.screens.buy_tickets.BuyTicketsScreen
 import com.example.welcomecompose.presentation.screens.home.HomeScreen
@@ -25,7 +31,11 @@ fun Navigation(
 
     Scaffold(
         bottomBar = {
-            AnimatedVisibility(visible = currentRoute == Screens.Home.route) {
+            AnimatedVisibility(
+                visible = currentRoute == Screens.Home.route,
+                enter =  slideInVertically() + fadeIn(),
+                exit = slideOutVertically { it } + fadeOut()
+            ) {
                 CustomBottomNav { navController.navigate(Screens.BuyTickets.route) }
             }
         },
@@ -49,4 +59,10 @@ sealed class Screens(val route: String) {
     object Booking : Screens("booking")
     object BuyTickets : Screens("buy_tickets")
     object Home : Screens("home")
+}
+
+@Preview
+@Composable
+fun PreviewNavigation() {
+    Navigation(rememberNavController())
 }
